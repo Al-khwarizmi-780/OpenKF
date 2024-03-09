67a14835ede6a77f65525bca7bb72882fc5938e4
Mohanad Youssef <info.codingcorner@gmail.com>
2024-03-10 00:00:50 +0100

    update bootstrap file

--- bootstrap-openkf.bat
+++ bootstrap-openkf.bat
@@ -1,16 +1,20 @@
 @echo off
 
-echo Creating Build Folder
-md .\cpp\build
+if not exist ".\cpp\build" (
+    echo Creating ./cpp/build Folder
+    md ./cpp/build
+) else (
+	echo ./cpp/build folder already exists
+)
 
 echo generating meta files
-cmake -S .\cpp -B .\cpp\build
+cmake -S ./cpp -B ./cpp/build
 
 echo building ...
-cmake --build .\cpp\build
+cmake --build ./cpp/build
 
 echo installing ...
-cmake --install .\cpp\build --config Debug
+cmake --install ./cpp/build --config Debug
 ::runas /user:Administrator "cmake --install .\cpp\build --config Debug"
 
 pause
