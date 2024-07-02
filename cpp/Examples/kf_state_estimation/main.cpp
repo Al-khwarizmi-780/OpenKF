8b917ec2ecbe43c60eab9db59f77a79117e787a8
Mohanad Youssef <info.codingcorner@gmail.com>
2024-07-02 12:51:45 +0200

    Create unscented transformation python

--- cpp/Examples/kf_state_estimation/main.cpp
+++ cpp/Examples/kf_state_estimation/main.cpp
@@ -1,18 +1,24 @@
 ///
-/// @author Mohanad Youssef
+/// Copyright 2022 CodingCorner
+///
+/// Use of this source code is governed by an MIT - style
+/// license that can be found in the LICENSE file or at
+/// https ://opensource.org/licenses/MIT.
+///
+/// @author Mohanad Youssef <mohanad.magdy.hammad@gmail.com>
 /// @file main.cpp
 ///
 
 #include <iostream>
 #include <stdint.h>
 
-#include "KalmanFilter/Types.h"
-#include "KalmanFilter/KalmanFilter.h"
+#include "kalman_filter/types.h"
+#include "kalman_filter/kalman_filter.h"
 
 static constexpr size_t DIM_X{ 2 };
 static constexpr size_t DIM_Z{ 1 };
-static constexpr kf::float32_t T{ 1.0F };
-static constexpr kf::float32_t Q11{ 0.1F }, Q22{ 0.1F };
+static constexpr float32_t T{ 1.0F };
+static constexpr float32_t Q11{ 0.1F }, Q22{ 0.1F };
 
 static kf::KalmanFilter<DIM_X, DIM_Z> kalmanfilter;
 
@@ -32,10 +38,10 @@ void executePredictionStep()
     kalmanfilter.vecX() << 0.0F, 2.0F;
     kalmanfilter.matP() << 0.1F, 0.0F, 0.0F, 0.1F;
 
-    kf::Matrix<DIM_X, DIM_X> F; // state transition matrix
+    Matrix<DIM_X, DIM_X> F; // state transition matrix
     F << 1.0F, T, 0.0F, 1.0F;
 
-    kf::Matrix<DIM_X, DIM_X> Q; // process noise covariance
+    Matrix<DIM_X, DIM_X> Q; // process noise covariance
     Q(0, 0) = (Q11 * T) + (Q22 * (std::pow(T, 3) / 3.0F));
     Q(0, 1) = Q(1, 0) = Q22 * (std::pow(T, 2) / 2.0F);
     Q(1, 1) = Q22 * T;
@@ -48,13 +54,13 @@ void executePredictionStep()
 
 void executeCorrectionStep()
 {
-    kf::Vector<DIM_Z> vecZ;
+    Vector<DIM_Z> vecZ;
     vecZ << 2.25F;
 
-    kf::Matrix<DIM_Z, DIM_Z> matR;
+    Matrix<DIM_Z, DIM_Z> matR;
     matR << 0.01F;
 
-    kf::Matrix<DIM_Z, DIM_X> matH;
+    Matrix<DIM_Z, DIM_X> matH;
     matH << 1.0F, 0.0F;
 
     kalmanfilter.correct(vecZ, matR, matH);
---
+++ cpp/kalman_filter/types.h
@@ -0,0 +1,26 @@
+///
+/// Copyright 2022 CodingCorner
+///
+/// Use of this source code is governed by an MIT - style
+/// license that can be found in the LICENSE file or at
+/// https ://opensource.org/licenses/MIT.
+///
+/// @author Mohanad Youssef <mohanad.magdy.hammad@gmail.com>
+/// @file types.h
+///
+
+#ifndef __KALMAN_FILTER_TYPES_H__
+#define __KALMAN_FILTER_TYPES_H__
+
+#include <stdint.h>
+#include <Eigen/Dense>
+
+using float32_t = float;
+
+template<size_t ROW, size_t COL>
+using Matrix = Eigen::Matrix<float32_t, ROW, COL>;
+
+template<size_t ROW>
+using Vector = Eigen::Matrix<float32_t, ROW, 1>;
+
+#endif // __KALMAN_FILTER_TYPES_H__
